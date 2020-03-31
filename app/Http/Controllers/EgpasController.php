01c84b86b4a6e9aab5072643b6e0cb524927c568
dujianjun <v@jianwi.cn>
2020-03-31 23:24:05 +0800

    创建控制器

---
+++ app/Http/Controllers/EgpasController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EgpasController extends Controller
+{
+    //
+    public function index()
+    {
+
+    }
+    public function show()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+}
